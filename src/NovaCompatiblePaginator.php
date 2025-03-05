6636fe3d2c0a625a12036a57d298c5ad33e4dbb0
Fawaz ADISA <adisaf@programmer.net>
2025-03-05 15:58:59 +0000

    update 1

--- src/NovaCompatiblePaginator.php
+++ src/NovaCompatiblePaginator.php
@@ -20,15 +20,20 @@ class NovaCompatiblePaginator extends LengthAwarePaginator
         $array = parent::toArray();
 
         // Assurez-vous que total est bien un nombre entier
-        $array['total'] = (int) $array['total'];
+        $array['total'] = (int)$array['total'];
 
-        // Nova recherche parfois ces clés supplémentaires
-        if (! isset($array['per_page'])) {
-            $array['per_page'] = (int) $this->perPage();
+        // S'assurer que les propriétés essentielles existent
+        if (!isset($array['per_page'])) {
+            $array['per_page'] = (int)$this->perPage();
         }
 
-        if (! isset($array['current_page'])) {
+        if (!isset($array['current_page'])) {
             $array['current_page'] = $this->currentPage();
+        }
+
+        // Ajouter total_pages si ce n'est pas déjà fait
+        if (!isset($array['total_pages']) && isset($array['last_page'])) {
+            $array['total_pages'] = $array['last_page'];
         }
 
         return $array;
